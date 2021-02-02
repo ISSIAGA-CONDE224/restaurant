08e69f70e86cd8cc4aab2f2da1927bcba2438fe4
ISSIAGA-CONDE224 <condeissiaga259@gmail.com>
2021-02-02 10:37:24 +0000

    suppression des commentaire unutils

--- lib/main.dart
+++ lib/main.dart
@@ -98,15 +98,6 @@ class _MyAppState extends State<MyApp> {
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(_toggleFavorite, _isMealFavorite),
         FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
       },
-      onGenerateRoute: (settings) {
-        print(settings.arguments);
-        // if (settings.name == '/meal-detail') {
-        //   return ...;
-        // } else if (settings.name == '/something-else') {
-        //   return ...;
-        // }
-        // return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
-      },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
           builder: (ctx) => CategoriesScreen(),
